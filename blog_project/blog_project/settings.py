60bf48bce612ffd86ad68ca4dc720445e10ea93d
pavansaigadde <pavansaigadde9@gmail.com>
2021-04-19 10:32:41 +0530

    new changes

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR=os.path.join(BASE_DIR,'templates')
@@ -21,10 +24,10 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='3n8@j7&pcmm_6v!xp^e4a_17f+)67bgqx2_7$vr955+t%^hvl_'
+SECRET_KEY =config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
