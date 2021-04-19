ed0434e3549f083aa9e11f00e416821868f1d9eb
pavansaigadde <pavansaigadde9@gmail.com>
2021-04-19 09:48:54 +0530

    new changes

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -21,10 +21,10 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY ='3n8@j7&pcmm_6v!xp^e4a_17f+)67bgqx2_7$vr955+t%^hvl_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
